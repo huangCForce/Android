3cff6eae58c0b97125ce177f8b96452f9e164d52
Tony Huang <huang690910789@gmail.com>
2016-09-29 16:55:03 +0800

    添加库文件目录

--- app/src/main/java/com/cforce/huang/android/views/NetWorkActivity.java
+++ app/src/main/java/com/cforce/huang/android/views/NetWorkActivity.java
@@ -1,8 +1,6 @@
 package com.cforce.huang.android.views;
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
